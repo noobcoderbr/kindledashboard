1698af8899a1d7d970a1fef4957e52a1297db332
noobcoderbr <lucas.lagrotta@gmail.com>
2026-06-17 11:12:20 -0300

    Refactor HTML generation in generate.js

--- generate.js
+++ generate.js
@@ -1,26 +1,39 @@
 const fs = require('fs');
 
-const html = `
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-<meta charset="UTF-8">
-<title>Kindle Concierge</title>
-</head>
-<body>
+async function run() {
 
-<h1>Olá, hóspede.</h1>
+    const response = await fetch(
+        'https://api.open-meteo.com/v1/forecast?latitude=-22.95&longitude=-43.18&current=temperature_2m'
+    );
 
-<p>Gerado automaticamente pelo GitHub Actions.</p>
+    const data = await response.json();
 
-<p>Data da geração:</p>
+    const temperatura = Math.round(
+        data.current.temperature_2m
+    );
 
-<p>${new Date().toLocaleString('pt-BR')}</p>
+    const html = `
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Kindle Concierge</title>
+    </head>
+    <body>
 
-</body>
-</html>
-`;
+        <h1>Olá, hóspede.</h1>
 
-fs.writeFileSync('index.html', html);
+        <h2>Clima agora</h2>
 
-console.log('Página criada.');
+        <p>${temperatura}°C</p>
+
+    </body>
+    </html>
+    `;
+
+    fs.writeFileSync('index.html', html);
+
+    console.log('Página criada');
+}
+
+run();
